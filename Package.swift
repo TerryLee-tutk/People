dfc606a6da31532d342280b12a95ce73e7dcfc5b
TerryLee <terry_lee@tutk.com>
2020-06-30 17:27:40 +0800

    Test

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "People",
+    platforms: [
+      .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -18,13 +21,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-//        .target(
-//            name: "People",
-//            dependencies: []),
-        .testTarget(
-            name: "PeopleTests",
-            dependencies: ["People"]),
-        .target(name: "People",
-                path: "Source")
+        .target(
+            name: "People",
+            dependencies: [])
     ]
 )
--- Tests/PeopleTests/PeopleTests.swift
+++ Tests/PeopleTests/PeopleTests.swift
@@ -6,7 +6,8 @@ final class PeopleTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(People().text, "Hello, World!")
+//        XCTAssertEqual(People().text, "Hello, World!")
+        XCTAssertEqual(People(name: "", gender: Gender.male, age: 0).text, "Hello, World!")
     }
 
     static var allTests = [
