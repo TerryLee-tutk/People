23aa301d4fe8c349b5750e29d02bf21eea9f91f0
TerryLee <terry_lee@tutk.com>
2020-06-30 16:36:04 +0800

    Add target path

--- Package.swift
+++ Package.swift
@@ -18,11 +18,13 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "People",
-            dependencies: []),
+//        .target(
+//            name: "People",
+//            dependencies: []),
         .testTarget(
             name: "PeopleTests",
             dependencies: ["People"]),
+        .target(name: "People",
+                path: "Source")
     ]
 )
